6c00cfb15697b5cc8dfb258facc628b73359667c
potew <potew0@gmail.com>
2020-12-07 17:59:34 -0300

    Flakeada

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,39 +1,10 @@
 from datetime import datetime
 from collections import Counter
 
-dados = [
-  {
-    "id": 1,
-    "nome_do_produto": "CALENDULA OFFICINALIS FLOWERING TOP, GERANIUM MACULATUM ROOT, SODIUM CHLORIDE, THUJA OCCIDENTALIS LEAFY TWIG, ZINC, and ECHINACEA ANGUSTIFOLIA",
-    "nome_da_empresa": "Forces of Nature",
-    "data_de_fabricacao": "2020-07-08",
-    "data_de_validade": "2023-02-09",
-    "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
-    "instrucoes_de_armazenamento": "in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices    phasellus"
-  },
-  {
-    "id": 2,
-    "nome_do_produto": "CALENDULA OFFICINALIS FLOWERING TOP, GERANIUM MACULATUM ROOT, SODIUM CHLORIDE, THUJA OCCIDENTALIS LEAFY TWIG, ZINC, and ECHINACEA ANGUSTIFOLIA",
-    "nome_da_empresa": "ACME INC",
-    "data_de_fabricacao": "2020-06-04",
-    "data_de_validade": "2022-02-02",
-    "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
-    "instrucoes_de_armazenamento": "in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices    phasellus"
-  },
-  {
-    "id": 3,
-    "nome_do_produto": "CALENDULA OFFICINALIS FLOWERING TOP, GERANIUM MACULATUM ROOT, SODIUM CHLORIDE, THUJA OCCIDENTALIS LEAFY TWIG, ZINC, and ECHINACEA ANGUSTIFOLIA",
-    "nome_da_empresa": "Forces of Nature",
-    "data_de_fabricacao": "2020-06-04",
-    "data_de_validade": "2021-02-02",
-    "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
-    "instrucoes_de_armazenamento": "in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices    phasellus"
-  },
-]
 
 class SimpleReport:
     def __init__(self, data):
-            self.data = data
+        self.data = data
 
     def generate(self):
         oldest_mfg_date = min([
@@ -51,9 +22,7 @@ class SimpleReport:
         return(
             f"""Data de fabricação mais antiga: {oldest_mfg_date}
                 Data de validade mais próxima: {closest_exp_date}
-                Empresa com maior quantidade de produtos estocados: {company_name[0]}
-            """
+                Empresa com maior quantidade de produtos estocados: {
+                  company_name[0]
+            }"""
         )
-
-
-SimpleReport(dados).generate()
