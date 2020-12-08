fde7868f9bce19dd662a26695ab140d698be8c4b
Marco Barbosa <marco.meireles.b@gmail.com>
2020-12-08 17:14:58 -0300

    Requisito 6

---
+++ inventory_report/importer/importer.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+class Importer(ABC):
+  def build(self):
+
+  @abstractmethod
+  def CsvImporter(self):
+  
+  def CsvImporter(self):
+  
+  def CsvImporter(self):
+
