8aaf4671bb99a8e1a14e8cd49f898eaa49e508bc
Marco Barbosa <marco.meireles.b@gmail.com>
2020-12-08 17:05:17 -0300

    ok

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,6 +2,7 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
+import xml.etree.ElementTree as ET
 
 
 class Inventory:
