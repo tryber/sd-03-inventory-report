1186467f1723a70680399e03c194a567e7a2b6a7
Gustaff <gustavohmfigueiredo@gmail.com>
2020-12-08 17:40:47 -0300

    bonus ok

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,3 +1,5 @@
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
 from collections.abc import Iterable
 from inventory_report.inventory.inventory_iterator import InventoryIterator
 
@@ -7,8 +9,15 @@ class InventoryRefactor(Iterable):
         self.importer = importer
         self.data = []
 
-    def import_data(self, file_name, mode):
+    def import_data(self, file_name):
         self.data += self.importer.import_data(file_name)
+
+    def generate(self, mode):
+        if mode == "simples":
+            relatory = SimpleReport.generate(self.data)
+        elif mode == "completo":
+            relatory = CompleteReport.generate(self.data)
+        return relatory
 
     def __iter__(self):
         return InventoryIterator(self.data)
--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,22 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+
 def main():
-    pass
+    try:
+        _, file_name, mode = sys.argv
+    except ValueError:
+        return print("Verifique os argumentos", file=sys.stderr)
+
+    if file_name.endswith(".json"):
+        inventory = InventoryRefactor(JsonImporter)
+    elif file_name.endswith(".csv"):
+        inventory = InventoryRefactor(CsvImporter)
+    elif file_name.endswith(".xml"):
+        inventory = InventoryRefactor(XmlImporter)
+
+    inventory.import_data(file_name)
+    print(inventory.generate(mode))
