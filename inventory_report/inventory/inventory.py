47a79d20a487608bc2b27a5cb19c96e201fd6d26
potew <potew0@gmail.com>
2020-12-09 09:16:43 -0300

    Complexity reduction

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -5,40 +5,43 @@ import json
 import csv
 
 
-def treatxml(file_path):
-    with open(f"../{file_path}") as xml_file:
-        root = ET.parse(xml_file).getroot()
-        records = root.findall('record')
-        output = []
-        for record in records:
-            dictionary = {}
-            for element in record:
-                dictionary[element.tag] = element.text
-            output.append(dictionary)
-        return output
-
-
 class Inventory:
     def __init__(self, file_path, report_type):
         self.file_path = file_path
         self.report_type = report_type
 
+    @staticmethod
+    def treatcsv(path):
+        with open(f"../{path}") as csv_file:
+            csv_dict = csv.DictReader(csv_file, delimiter=",")
+            output = []
+            for dict in csv_dict:
+                output.append(dict)
+            return output
+
+    @staticmethod
+    def treatxml(path):
+        with open(f"../{path}") as xml_file:
+            root = ET.parse(xml_file).getroot()
+            records = root.findall('record')
+            output = []
+            for record in records:
+                dictionary = {}
+                for element in record:
+                    dictionary[element.tag] = element.text
+                output.append(dictionary)
+            return output
+
     @classmethod
     def import_data(self, file_path, report_type):
 
         if (file_path.endswith(".csv")):
-            with open(f"../{file_path}") as csv_file:
-                csv_dict = csv.DictReader(csv_file, delimiter=",")
-                output = []
-                for dict in csv_dict:
-                    output.append(dict)
-
-        if (file_path.endswith(".json")):
+            output = Inventory.treatcsv(file_path)
+        elif (file_path.endswith(".json")):
             with open(f"../{file_path}") as json_file:
                 output = json.load(json_file)
-
-        if (file_path.endswith(".xml")):
-            output = treatxml(file_path)
+        elif (file_path.endswith(".xml")):
+            output = Inventory.treatxml(file_path)
 
         if report_type == 'simples':
             return(SimpleReport.generate(output))
