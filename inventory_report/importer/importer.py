38e1b2cd24cace7febf2f9fc7ef91220d28e8a5a
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-12-11 02:18:28 -0300

    2 tests

--- inventory_report/importer/importer.py
+++ inventory_report/importer/importer.py
@@ -1,18 +1,7 @@
 from abc import ABC, abstractmethod
-from inventory_report.importer.csv_importer import CsvImporter
-from inventory_report.importer.json_importer import JsonImporter
-from inventory_report.importer.xml_importer import XmlImporter
 
 
 class Importer(ABC):
     @abstractmethod
-    def CsvImporter(self):
-        raise NotImplementedError
-
-    @abstractmethod
-    def JsonImporter(self):
-        raise NotImplementedError
-
-    @abstractmethod
-    def XmlImporter(self):
+    def xablau(self):
         raise NotImplementedError
