defcc40c27bfc238d505899a934f61fd6cbb90fb
clayton pereira <cpereiramt@gmail.com>
2020-12-20 14:49:10 -0400

    salvando alteraçoes no requisito 6

---
+++ inventory_report/importer/csv_importer.py
@@ -0,0 +1,6 @@
+from inventory_report.importer.importer import Importer
+
+
+class CsvImporter(Importer):
+    def import_data(self):
+        return self
---
+++ inventory_report/importer/importer.py
@@ -0,0 +1,8 @@
+import abc
+
+
+class Importer:
+    @abc.abstractmethod
+    def import_data(self):
+        pass
+
