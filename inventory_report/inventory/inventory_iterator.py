c302d12d75d9037dd4bdd12c06087b89512f1ce9
lcdlima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2021-01-03 18:29:47 -0300

    Doing inventory_iterator.py

--- inventory_report/inventory/inventory_iterator.py
+++ inventory_report/inventory/inventory_iterator.py
@@ -1,3 +1,16 @@
-class InventoryIterator:
-    def __init__(self):
-        self.status = 'ok'
+from collections.abc import Iterator
+
+
+class InventoryIterator(Iterator):
+    def __init__(self, iterable):
+        self._iterable = iterable
+        self._position = 0
+
+    def __next__(self):
+        try:
+            currentValue = self._iterable[self._position]
+        except IndexError:
+            raise StopIteration()
+        else:
+            self._position += 1
+            return currentValue
