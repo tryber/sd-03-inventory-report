2ddf96355d03f1a3c29630d5a4bcdbcca3f21beb
DiegoRafaelAF <draf0314@gmail.com>
2021-01-05 13:11:11 -0300

    correçao erros

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -4,29 +4,6 @@ from collections import Counter
 
 class SimpleReport:
     @staticmethod
-    def generate(list):
-        oldest = list[0]["data_de_fabricacao"]
-        closest = list[0]["data_de_validade"]
-        biggest_company = ""
-        biggest_company_value = 0
-        name_company = {}
-        for product in list:
-            closest, oldest = parse_list(
-                product, closest, oldest, name_company
-            )
-
-        for company, quantity in name_company.items():
-            if quantity > biggest_company_value:
-                biggest_company_value = quantity
-                biggest_company = company
-
-        relatory = (
-            f"Data de fabricação mais antiga: {oldest}\n"
-            + f"Data de validade mais próxima: {closest}\n"
-            + "Empresa com maior quantidade de produtos "
-            + f"estocados: {biggest_company}\n"
-        )
-        return relatory
     def company_counter(self, prodList):
         companies = [elem["nome_da_empresa"] for elem in prodList]
         companies_counter = Counter(companies)
