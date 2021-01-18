ad78a23c3935f4b34b8c2f30f09fbcb6a2e1b92a
Mateus Talles <mateustalles@gmail.com>
2021-01-18 19:50:36 -0300

    linting

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,13 +1,13 @@
 from inventory_report.reports.simple_report import SimpleReport
 from collections import Counter
-from operator import itemgetter
 
 
 class CompleteReport(SimpleReport):
     @classmethod
     def generate(cls, reports):
         simple_report = super().generate(reports)
-        stocked_products = list(Counter(report["nome_da_empresa"] for report in reports).items())
+        stocked_products = list(Counter(report["nome_da_empresa"]
+                                        for report in reports).items())
         complete_report = f"""{simple_report}
 Produtos Estocados por empresa: \n"""
         for product in stocked_products:
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
 from collections import Counter
-from operator import itemgetter
 
 
 class Report(ABC):
@@ -10,26 +9,37 @@ class Report(ABC):
     def generate(cls, reports):
         raise NotImplementedError
 
+
 class SimpleReport(Report):
     @classmethod
     def generate(cls, reports):
         def oldest_fabrication_date_sort(report):
-            fabrication_date = datetime.strptime(report["data_de_fabricacao"], '%Y-%m-%d')
+            fabrication_date = datetime.strptime(
+                report["data_de_fabricacao"], '%Y-%m-%d'
+            )
             return fabrication_date
 
         def closest_validity_date_sort(report):
-            validity_datetime = datetime.strptime(report["data_de_validade"], '%Y-%m-%d')
+            validity_datetime = datetime.strptime(
+                report["data_de_validade"], '%Y-%m-%d'
+            )
             current_datetime = datetime.now()
             datetime_difference = validity_datetime - current_datetime
             if(datetime_difference.total_seconds() < 0):
                 return (1, datetime_difference)
             return (0, datetime_difference)
 
-        oldest_fabrication_date = sorted(reports, key=oldest_fabrication_date_sort)[0]["data_de_fabricacao"]
-        closest_validity_date = sorted(reports, key=closest_validity_date_sort)[0]["data_de_validade"]
-        company_with_most_products = Counter(report["nome_da_empresa"] for report in reports).most_common(1)[0][0]
+        oldest_fabrication_date = sorted(
+            reports, key=oldest_fabrication_date_sort
+        )[0]["data_de_fabricacao"]
+        closest_validity_date = sorted(
+            reports, key=closest_validity_date_sort
+        )[0]["data_de_validade"]
+        company_products = Counter(
+            report["nome_da_empresa"] for report in reports
+        ).most_common(1)[0][0]
 
         return f"""Data de fabricação mais antiga: {oldest_fabrication_date}
 Data de validade mais próxima: {closest_validity_date}
-Empresa com maior quantidade de produtos estocados: {company_with_most_products}
+Empresa com maior quantidade de produtos estocados: {company_products}
 """
